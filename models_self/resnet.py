862bc7ca2581a8e89b5c4d68302c83a7e73d804e
wuzhiwen001 <youyuehanxiao@163.com>
2023-07-13 23:26:34 +0800

    添加注释

--- models_self/resnet.py
+++ models_self/resnet.py
@@ -3,36 +3,55 @@ import torch
 
 
 class BasicBlock(nn.Module):
-    expansion = 1
+    '''
+    1. 基本块，非瓶颈结构，没有升维和降维操作
+    2. 包含两层卷积层，每一层的卷积核尺寸都是3×3，第二层步长固定为1
+    3. 每一层之后都进行批归一化
+    '''
+
+    expansion = 1 #通道扩展倍数（瓶颈结构大于1）
 
     def __init__(self, in_channel, out_channel, stride=1, downsample=None, **kwargs):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channel)
-        self.relu = nn.ReLU()
+        #第一层
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=3, stride=stride, padding=1, bias=False) #卷积对象实例
+        self.bn1 = nn.BatchNorm2d(out_channel) #批归一化实例
+        self.relu = nn.ReLU() #激活实例
+        #第二层
         self.conv2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channel)
-        self.downsample = downsample
+
+        self.downsample = downsample #进行残差连接时，对原始输入特征所做的操作（为了保证与输出一致），为None表示不做任何操作
 
     def forward(self, x):
         identity = x
+        #是否需要对输入进行处理（最后会与卷积输出相加）
         if self.downsample is not None:
             identity = self.downsample(x)
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
+        #第一层计算
+        out = self.conv1(x) #卷积操作
+        out = self.bn1(out) #批归一化操作
+        out = self.relu(out) #激活操作
 
+        #第二层
         out = self.conv2(out)
         out = self.bn2(out)
 
+        #残差连接
         out += identity
+        #激活
         out = self.relu(out)
 
         return out
 
 
 class Bottleneck(nn.Module):
+    '''
+    1. 瓶颈结构块，通过点卷积进行将维和升维
+    2. 一共三层，第一层点卷积减少通道数（减少计算量），第二层进行标准卷积，第三层点卷积增加通道数（提取更多的特征）
+    3. 每一层之后都有批归一化
+    '''
     """
     注意:原论文中,在虚线残差结构的主分支上,第一个1x1卷积层的步距是2,第二个3x3卷积层步距是1。
     但在pytorch官方实现过程中是第一个1x1卷积层的步距是1,第二个3x3卷积层步距是2,
@@ -45,42 +64,54 @@ class Bottleneck(nn.Module):
                  groups=1, width_per_group=64):
         super(Bottleneck, self).__init__()
 
-        width = int(out_channel * (width_per_group / 64.)) * groups
+        width = int(out_channel * (width_per_group / 64.)) * groups #分组处理
 
-        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=width,  kernel_size=1, stride=1, bias=False)  # squeeze channels
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=width,  kernel_size=1, stride=1, bias=False)  # squeeze channels（减少通道数）
         self.bn1 = nn.BatchNorm2d(width)
         # -----------------------------------------
-        self.conv2 = nn.Conv2d(in_channels=width, out_channels=width, groups=groups, kernel_size=3, stride=stride, bias=False, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=width, out_channels=width, groups=groups, kernel_size=3, stride=stride, bias=False, padding=1) #卷积
         self.bn2 = nn.BatchNorm2d(width)
         # -----------------------------------------
-        self.conv3 = nn.Conv2d(in_channels=width, out_channels=out_channel*self.expansion, kernel_size=1, stride=1, bias=False)  # unsqueeze channels
+        self.conv3 = nn.Conv2d(in_channels=width, out_channels=out_channel*self.expansion, kernel_size=1, stride=1, bias=False)  # unsqueeze channels（增加通道数）
         self.bn3 = nn.BatchNorm2d(out_channel*self.expansion)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace=True) #激活
+
         self.downsample = downsample
 
     def forward(self, x):
         identity = x
+        #对输入进行处理，保证与输出一致
         if self.downsample is not None:
             identity = self.downsample(x)
 
+        #第一层，降维
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
 
+        #第二层，卷积计算
         out = self.conv2(out)
         out = self.bn2(out)
         out = self.relu(out)
 
+        #第三层，升维
         out = self.conv3(out)
         out = self.bn3(out)
 
+        #残差连接
         out += identity
+
+        #激活操作
         out = self.relu(out)
 
         return out
 
 
 class ResNet(nn.Module):
+    '''
+    1. ResNet类，用于构建不同层的ResNet模型
+    2. blocks_num为一个序列，包含4个整型元素，每一个元素表示对应基本块的个数。通过设置blocks_num可以构建不同层数的ResNet网络
+    '''
 
     def __init__(self,
                  block,
@@ -89,6 +120,10 @@ class ResNet(nn.Module):
                  include_top=True,
                  groups=1,
                  width_per_group=64):
+        '''
+        :param block: 基础块（BasicBlock或Bottleneck）
+        :param blocks_num: 一个序列，设置每一个基础块使用的个数，以此创建不同层数的网络
+        '''
         super(ResNet, self).__init__()
         self.include_top = include_top
         self.in_channel = 64
@@ -96,36 +131,47 @@ class ResNet(nn.Module):
         self.groups = groups
         self.width_per_group = width_per_group
 
+        #首部处理，减小输入特征尺寸
         self.conv1 = nn.Conv2d(3, self.in_channel, kernel_size=7, stride=2,  padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(self.in_channel)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace=True) #inplace表示是否将原来值直接替换为激活后的值（不用中间变量进行存储）
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        #创建中间卷积层
         self.layer1 = self._make_layer(block, 64, blocks_num[0])
         self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)
         self.layer3 = self._make_layer(block, 256, blocks_num[2], stride=2)
         self.layer4 = self._make_layer(block, 512, blocks_num[3], stride=2)
+
+        #全连接，进行评估
         if self.include_top:
-            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  #output size = (1, 1)
+            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  #output size = (1, 1) #自适应平均池化
             self.fc = nn.Linear(512 * block.expansion, num_classes)
 
+        #权重初始化
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
 
     def _make_layer(self, block, channel, block_num, stride=1):
-        downsample = None
+        downsample = None #输入处理函数
+
+        #输入通道与输出通道不相等，对输入通道进行增加或减少处理（保证正确地进行残差连接）
         if stride != 1 or self.in_channel != channel * block.expansion:
             downsample = nn.Sequential(
                 nn.Conv2d(self.in_channel, channel * block.expansion, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(channel * block.expansion))
 
         layers = []
+        #当前layer的第一个基础块
         layers.append(block(self.in_channel,
                             channel,
                             downsample=downsample,
                             stride=stride,
                             groups=self.groups,
                             width_per_group=self.width_per_group))
+
+        #第一个基础块结束后，其输出为下一个基础块的输入（后面几个块中的第一层输入的通道数均相同）
         self.in_channel = channel * block.expansion
 
         for _ in range(1, block_num):
@@ -134,22 +180,26 @@ class ResNet(nn.Module):
                                 groups=self.groups,
                                 width_per_group=self.width_per_group))
 
+        #将当前layer中的块按顺序连接在一起（上一个块的输出为下一个块的输入）
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        #首部处理
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
 
+        #卷积层计算
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
 
+        #评估打分
         if self.include_top:
             x = self.avgpool(x)
-            x = torch.flatten(x, 1)
+            x = torch.flatten(x, 1) #前1维不变，后面维度展平
             x = self.fc(x)
 
         return x
--- train_sample.py
+++ train_sample.py
@@ -7,14 +7,16 @@ from torchvision import transforms, datasets
 import torch.optim as optim 
 from tqdm import tqdm  
 #from classic_models.alexnet import AlexNet
-from classic_models.googlenet_v1 import  GoogLeNet
+#from classic_models.googlenet_v1 import  GoogLeNet
+import models_self.resnet as R
 def main():
     # 判断可用设备
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("using {} device.".format(device))
 
     # 注意改成自己的数据集路径
-    data_path = '/mnt/d/Datasets/flower'
+    #data_path = '/mnt/d/Datasets/flower'
+    data_path = 'D:/Machine learning/datasets/flower'
     assert os.path.exists(data_path), "{} path does not exist.".format(data_path) 
 
     # 数据预处理与增强
@@ -36,7 +38,7 @@ def main():
 
 
     # 使用ImageFlolder加载数据集中的图像，并使用指定的预处理操作来处理图像， ImageFlolder会同时返回图像和对应的标签。 (image path, class_index) tuples
-    train_dataset = datasets.ImageFolder(root=os.path.join(data_path, "train"), transform=data_transform["train"])
+    train_dataset = datasets.ImageFolder(root=os.path.join(data_path, "train"), transform=data_transform["train"]) #os.path.join(data_path, "train")为路径拼接
     validate_dataset = datasets.ImageFolder(root=os.path.join(data_path, "val"), transform=data_transform["val"])
     train_num = len(train_dataset)
     val_num = len(validate_dataset)
@@ -47,56 +49,58 @@ def main():
     cla_dict = dict((val, key) for key, val in flower_list.items())
     # 将字典写成一个json文件
     json_str = json.dumps(cla_dict, indent=4)
-    with open( os.path.join(data_path, 'class_indices.json') , 'w') as json_file:
+    with open(os.path.join(data_path, 'class_indices.json'), 'w') as json_file:
         json_file.write(json_str)
 
-    batch_size = 64 # batch_size大小，是超参，可调，如果模型跑不起来，尝试调小batch_size
+    batch_size = 16 #batch_size大小，是超参，可调，如果模型跑不起来，尝试调小batch_size
  
-    # 使用 DataLoader 将 ImageFloder 加载的数据集处理成批量（batch）加载模式
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True )
-    validate_loader = torch.utils.data.DataLoader(validate_dataset, batch_size=4, shuffle=False ) # 注意，验证集不需要shuffle
+    #使用 DataLoader 将 ImageFloder 加载的数据集处理成批量（batch）加载模式
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    validate_loader = torch.utils.data.DataLoader(validate_dataset, batch_size=4, shuffle=False) #注意，验证集不需要shuffle
     print("using {} images for training, {} images for validation.".format(train_num, val_num))
     
     # 实例化模型，并送进设备
-    net = GoogLeNet(num_classes = 5)
-    #net = AlexNet(num_classes=5 )
+    #net = GoogLeNet(num_classes=5)
+    net = R.resnet34(num_classes=5)
+    #net = AlexNet(num_classes=5)
     net.to(device)
 
     # 指定损失函数用于计算损失；指定优化器用于更新模型参数；指定训练迭代的轮数，训练权重的存储地址
-    loss_function = nn.CrossEntropyLoss() # MSE
+    loss_function = nn.CrossEntropyLoss() #MSE
     optimizer = optim.Adam(net.parameters(), lr=0.0002)
     epochs = 70
-    save_path = os.path.abspath(os.path.join(os.getcwd(), './results/weights/alexnet')) 
+    save_path = os.path.abspath(os.path.join(os.getcwd(), './results/weights/resnet'))
     if not os.path.exists(save_path):    
-        os.makedirs(save_path)
+        os.makedirs(save_path) #递归创建目录
 
-    best_acc = 0.0 # 初始化验证集上最好的准确率，以便后面用该指标筛选模型最优参数。  
+    best_acc = 0.0 #初始化验证集上最好的准确率，以便后面用该指标筛选模型最优参数。
     for epoch in range(epochs):
         ############################################################## train ######################################################
         net.train() 
-        acc_num = torch.zeros(1).to(device)    # 初始化，用于计算训练过程中预测正确的数量
-        sample_num = 0                         # 初始化，用于记录当前迭代中，已经计算了多少个样本
-        # tqdm是一个进度条显示器，可以在终端打印出现在的训练进度
+        acc_num = torch.zeros(1).to(device)    #初始化，用于计算训练过程中预测正确的数量
+        sample_num = 0                         #初始化，用于记录当前迭代中，已经计算了多少个样本
+        #tqdm是一个进度条显示器，可以在终端打印出现在的训练进度
         train_bar = tqdm(train_loader, file=sys.stdout, ncols=100)
-        for data in train_bar :
+        for data in train_bar:
             images, labels = data 
             sample_num += images.shape[0] #[64, 3, 224, 224]
             optimizer.zero_grad()
-            outputs = net(images.to(device)) # output_shape: [batch_size, num_classes]
-            pred_class = torch.max(outputs, dim=1)[1] # torch.max 返回值是一个tuple，第一个元素是max值，第二个元素是max值的索引。
-            acc_num += torch.eq(pred_class, labels.to(device)).sum() 
-            loss = loss_function(outputs, labels.to(device)) # 求损失
-            loss.backward() # 自动求导
-            optimizer.step() # 梯度下降
+            outputs = net(images.to(device)) #output_shape: [batch_size, num_classes]
+            #print(torch.max(outputs, dim=1))
+            pred_class = torch.max(outputs, dim=1)[1] #取预测类别的标签。torch.max 返回值是一个tuple，第一个元素是max值，第二个元素是max值的索引。
+            acc_num += torch.eq(pred_class, labels.to(device)).sum() #同一位置元素相同为True，不同为False
+            loss = loss_function(outputs, labels.to(device)) #求损失
+            loss.backward() #自动求导
+            optimizer.step() #梯度下降
 
-            # print statistics 
+            #print statistics
             train_acc = acc_num.item() / sample_num 
-            # .desc是进度条tqdm中的成员变量，作用是描述信息
+            #.desc是进度条tqdm中的成员变量，作用是描述信息
             train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,  epochs, loss)
 
         # validate
         net.eval()
-        acc_num = 0.0  # accumulate accurate number per epoch
+        acc_num = 0.0  #accumulate accurate number per epoch
         with torch.no_grad(): 
             for val_data in validate_loader:
                 val_images, val_labels = val_data
@@ -105,19 +109,18 @@ def main():
                 acc_num += torch.eq(predict_y, val_labels.to(device)).sum().item() 
 
         val_accurate = acc_num / val_num
-        print('[epoch %d] train_loss: %.3f  train_acc: %.3f  val_accuracy: %.3f' %  (epoch + 1, loss, train_acc, val_accurate))   
-        # 判断当前验证集的准确率是否是最大的，如果是，则更新之前保存的权重
+        print('[epoch %d] train_loss: %.3f  train_acc: %.3f  val_accuracy: %.3f' %(epoch + 1, loss, train_acc, val_accurate))
+        #判断当前验证集的准确率是否是最大的，如果是，则更新之前保存的权重
         if val_accurate > best_acc:
             best_acc = val_accurate
-            torch.save(net.state_dict(), os.path.join(save_path, "AlexNet.pth") )
+            torch.save(net.state_dict(), os.path.join(save_path, "Resnet.pth"))
 
-        # 每次迭代后清空这些指标，重新计算 
+        #每次迭代后清空这些指标，重新计算
         train_acc = 0.0
         val_accurate = 0.0
 
     print('Finished Training')
 
- 
-# if __name__ == '__main__':
-#     main()
-main()
+
+if __name__ == '__main__':
+    main()
