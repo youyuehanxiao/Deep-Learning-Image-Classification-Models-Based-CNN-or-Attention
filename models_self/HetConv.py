c27c586e2cbec6b0adc3907508bec82faea1f08f
wuzhiwen001 <youyuehanxiao@163.com>
2023-07-17 14:45:35 +0800

    另一种异构实现

--- models_self/HetConv.py
+++ models_self/HetConv.py
@@ -1,40 +1,43 @@
 '''
-创建一个输入通道是in_channel、输出通道是out_channel、保留 K×K 尺寸通道数为 1/P 的异构卷积层的主要思路：
-1. 总共是要创建out_channel个滤波器，每个滤波器的通道数是in_channel。对于每一个滤波，进行逐通道创建。
+一、创建一个输入通道是in_channels、输出通道是out_channels、保留 K×K 尺寸通道数为 1/P 的异构卷积层的主要思路：（HetConv）
+1. 总共是要创建out_channels个滤波器，每个滤波器的通道数是in_channels。对于每一个滤波，进行逐通道创建。
 2. 对于第i个滤波器，第j个通道有：
-    当 j = (i + P * K_count) % in_channel 且 K_count＜in_channel/P，其中K_count为当前滤波器已经创建得到尺寸为K的通道数时，有
+    当 j = (i + P * K_count) % in_channels 且 K_count＜in_channels/P，其中K_count为当前滤波器已经创建得到尺寸为K的通道数时，有
     H(ij) = nn.Conv2d(1, 1, kernel_size=K, stride=stride, padding=pd, bias=False) #pd = 1/2 * (K -1) K为奇数
     当j为其它值时，有
     H(ij) = nn.Conv2d(1, 1, kernel_size=1, stride=stride, padding=0, bias=False)
 3. 将所有滤波器存放到一个二维列表filters_list中，且元素filters_list[i][j]表示第i个滤波器，第j个通道
 4. 在进行卷积运算时，每一个滤波的各个通道分别与输入样本的对应通道进行卷积运算，然后将运算结果进行相加；不同滤波器的卷积结果进行通道拼接；最终得到异构卷积的结果。
+
+二、思路二：（Efficient_HetConv）
+异构卷积的结果 = 分P组的K卷积 + 不分组的点卷积 - 分P组的点卷积
 '''
 import torch.nn as nn
 import torch
 class HetConv(nn.Module):
-    def __init__(self, in_channel:int, out_channel:int, P:int, kernel_size:int, stride:int, bias=False):
+    def __init__(self, in_channels:int, out_channels:int, P:int, kernel_size:int, stride:int, bias=False):
         '''
-        :param in_channel: 输入通道数
-        :param out_channel: 输出通道数（滤波器个数）
-        :param P: 每一个滤波器使用 1/P * in_channel个尺寸为Kernel_size的滤波器
+        :param in_channels: 输入通道数
+        :param out_channels: 输出通道数（滤波器个数）
+        :param P: 每一个滤波器使用 1/P * in_channels个尺寸为Kernel_size的滤波器
         :param kernel_size: 使用的卷积核尺寸（不能于1）
         :param bias:是否使用偏置参数
         '''
         super(HetConv, self).__init__()
-        self.filters_list = self.create_filters(in_channel, out_channel, P, kernel_size, stride, bias)
+        self.filters_list = self.create_filters(in_channels, out_channels, P, kernel_size, stride, bias)
 
-    def create_filters(self, in_channel:int, out_channel:int, P:int, kernel_size:int, stride:int, bias=False) -> list:
-        filters_list = list() #存储异构卷积滤波器，filters_list[i][j]表示第i个滤波器，第j个通道
+    def create_filters(self, in_channels:int, out_channels:int, P:int, kernel_size:int, stride:int, bias=False) -> nn.ModuleList:
+        filters_list = nn.ModuleList() #存储异构卷积滤波器，filters_list[i][j]表示第i个滤波器，第j个通道
 
         #创建异构卷积滤波器
-        for i in range(out_channel):
-            filters_list_i = list()
+        for i in range(out_channels):
+            filters_list_i = nn.ModuleList()
             #创建第i个滤波器的每个通道（从第i个通道开始，每隔P个通道创建一个尺寸为kernel_size的通道）
-            first_K = i % in_channel #第一个尺寸为K的通道序号
-            for j in range(in_channel):
+            first_K = i % in_channels #第一个尺寸为K的通道序号
+            for j in range(in_channels):
                 conv = None #当前通道
                 if j < first_K:
-                    if (in_channel + j - first_K) % P == 0:
+                    if (in_channels + j - first_K) % P == 0:
                         conv = nn.Conv2d(1, 1, kernel_size=kernel_size, padding=int((kernel_size - 1) / 2),
                                          stride=stride, bias=bias) #创建尺寸为kernel_size的卷积核
                     else:
@@ -69,19 +72,55 @@ class HetConv(nn.Module):
             else:
                 out = torch.cat([out, out_i], dim=1)
 
-        print('out_size:', out.shape)
+        #print('out_size:', out.shape)
 
+        return out
+
+class Efficient_HetConv(nn.Module):
+    def __init__(self, in_channels:int, out_channels:int, P:int, kernel_size:int, stride:int, bias=False):
+        '''
+        :param in_channels: 输入通道数
+        :param out_channels: 输出通道数（滤波器个数）
+        :param P: 每一个滤波器使用 1/P * in_channels个尺寸为Kernel_size的滤波器
+        :param kernel_size: 使用的卷积核尺寸（不能于1）
+        :param bias:是否使用偏置参数
+        '''
+        super(Efficient_HetConv, self).__init__()
+
+        #创建异构卷积滤波器
+        if in_channels % P != 0:
+            raise ValueError('in_channels must be divisible by p')
+        if out_channels % P != 0:
+            raise ValueError('out_channels must be divisible by p')
+
+        # 分P组K卷积
+        self.conv3x3_P = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
+                                 padding=int((kernel_size - 1) / 2), groups=P, bias=bias)
+
+        # 分P组点卷积
+        self.conv1x1_P = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride,
+                                 padding=0, groups=P, bias=bias)
+
+        # 不分组点卷积
+        self.conv1x1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride,
+                                 padding=0, groups=1, bias=bias)
+
+    def forward(self, x):
+        out = self.conv3x3_P(x) + self.conv1x1(x) - self.conv1x1_P(x)
+        #print(out.shape)
         return out
 
 if __name__ == '__main__':
     imgs_1 = torch.rand((5, 64, 224, 224))
-    ht = HetConv(64, 128, 5, 5, 2)
-    for i, filter_i in enumerate(ht.filters_list):
-        print(f'第{i}个滤波器:')
-        for j, channel_j in enumerate(filter_i):
-            if channel_j.weight.shape[2] != 1:
-                print(f'第{j}个通道尺寸：', channel_j.weight.shape)
-        print('='*50)
-    ht(imgs_1)
+    # ht = HetConv(64, 128, 128, 5, 2)
+    # for i, filter_i in enumerate(ht.filters_list):
+    #     print(f'第{i}个滤波器:')
+    #     for j, channel_j in enumerate(filter_i):
+    #         if channel_j.weight.shape[2] != 1:
+    #             print(f'第{j}个通道尺寸：', channel_j.weight.shape)
+    #     print('='*50)
+    # ht(imgs_1)
+    ht2 = Efficient_HetConv(64, 64, 64, 7, 2)
+    ht2(imgs_1)
 
 
---
+++ models_self/hetconv_resnet.py
@@ -0,0 +1,318 @@
+'''
+具有异构卷积核的resnet（将基本块中的标准卷积替换为异构卷积）
+'''
+
+import torch.nn as nn
+import torch
+from models_self.HetConv import HetConv, Efficient_HetConv
+
+
+class BasicBlock(nn.Module):
+    '''
+    1. 基本块，非瓶颈结构，没有升维和降维操作
+    2. 包含两层卷积层，每一层的卷积核尺寸都是3×3和 1×1，第二层步长固定为1（保持输出尺寸不变）
+    3. 每一层之后都进行批归一化
+    '''
+
+    expansion = 1  #通道扩展倍数（若为瓶颈结构，该值大于1）
+
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None, **kwargs):
+        super(BasicBlock, self).__init__()
+        # 第一层
+        #print('创建第一层异构卷积...')
+        # self.conv1 = HetConv(in_channels=in_channel, out_channels=out_channel, P=out_channel,
+        #                      kernel_size=3, stride=stride, bias=False)  #创建异构卷积层（P=N，N为滤波器的个数)
+        self.conv1 = Efficient_HetConv(in_channels=in_channel, out_channels=out_channel, P=min(out_channel, in_channel),
+                             kernel_size=3, stride=stride, bias=False)  # 创建异构卷积层（P=N，N为滤波器的个数)
+        self.bn1 = nn.BatchNorm2d(out_channel)  # 批归一化实例
+        self.relu = nn.ReLU()  # 激活实例
+        # 第二层
+        #print('创建第二层异构卷积...')
+        # self.conv2 = HetConv(in_channels=out_channel, out_channels=out_channel, P=out_channel,
+        #                      kernel_size=3, stride=1, bias=False)
+        self.conv2 = Efficient_HetConv(in_channels=out_channel, out_channels=out_channel, P=min(out_channel, in_channel),
+                             kernel_size=3, stride=1, bias=False)  # 创建异构卷积层（P=N，N为滤波器的个数)
+        self.bn2 = nn.BatchNorm2d(out_channel)
+
+        self.downsample = downsample  #进行残差连接时，对原始输入特征所做的操作（为了保证与输出一致），为None表示不做任何操作
+
+    def forward(self, x):
+        identity = x
+        # 是否需要对输入进行处理（最后会与卷积输出相加）
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        #print('identity.size:', identity.shape)
+
+        # 第一层计算
+        # print('first_input_size:', x.shape)
+        # print('first_weight_size:', self.conv1.weight.shape)
+        out = self.conv1(x)  # 卷积操作
+        #print('first_out_size:', out.shape)
+        out = self.bn1(out)  # 批归一化操作
+        out = self.relu(out)  # 激活操作
+
+        # 第二层
+        # print('second_input_size:', out.shape)
+        # print('second_weight_size:', self.conv2.weight.shape)
+        out = self.conv2(out)
+        #print('second_out_size:', out.shape)
+        out = self.bn2(out)
+
+        # 残差连接
+        out += identity
+        # 激活
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    '''
+    1. 瓶颈结构块，通过点卷积进行将维和升维
+    2. 一共三层，第一层点卷积减少通道数（减少计算量），第二层进行标准卷积，第三层点卷积增加通道数（提取更多的特征）
+    3. 每一层之后都有批归一化
+    '''
+    """
+    注意:原论文中,在虚线残差结构的主分支上,第一个1x1卷积层的步距是2,第二个3x3卷积层步距是1。
+    但在pytorch官方实现过程中是第一个1x1卷积层的步距是1,第二个3x3卷积层步距是2,
+    这么做的好处是能够在top1上提升大概0.5%的准确率。
+    可参考Resnet v1.5 https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch
+    """
+    expansion = 4
+
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None,
+                 groups=1, width_per_group=64):
+        super(Bottleneck, self).__init__()
+
+        width = int(out_channel * (width_per_group / 64.)) * groups  # 分组处理
+
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=width, kernel_size=1, stride=1,
+                               bias=False)  # squeeze channels（减少通道数）
+        self.bn1 = nn.BatchNorm2d(width)
+        # -----------------------------------------
+        self.conv2 = nn.Conv2d(in_channels=width, out_channels=width, groups=groups, kernel_size=3, stride=stride,
+                               bias=False, padding=1)  # 卷积
+        self.bn2 = nn.BatchNorm2d(width)
+        # -----------------------------------------
+        self.conv3 = nn.Conv2d(in_channels=width, out_channels=out_channel * self.expansion, kernel_size=1, stride=1,
+                               bias=False)  # unsqueeze channels（增加通道数）
+        self.bn3 = nn.BatchNorm2d(out_channel * self.expansion)
+        self.relu = nn.ReLU(inplace=True)  # 激活
+
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+        # 对输入进行处理，保证与输出一致
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        # 第一层，降维
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        # 第二层，卷积计算
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        # 第三层，升维
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        # 残差连接
+        out += identity
+
+        # 激活操作
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+    '''
+    1. ResNet类，用于构建不同层的ResNet模型
+    2. blocks_num为一个序列，包含4个整型元素，每一个元素表示对应基本块的个数。通过设置blocks_num可以构建不同层数的ResNet网络
+    '''
+
+    def __init__(self,
+                 block,
+                 blocks_num,
+                 num_classes=1000,
+                 include_top=True,
+                 groups=1,
+                 width_per_group=64):
+        '''
+        :param block: 基础块（BasicBlock或Bottleneck）
+        :param blocks_num: 一个序列，设置每一个基础块使用的个数，以此创建不同层数的网络
+        '''
+        super(ResNet, self).__init__()
+        self.include_top = include_top
+        self.in_channel = 64
+
+        self.groups = groups
+        self.width_per_group = width_per_group
+
+        # 首部处理，减小输入特征尺寸，通道扩展到64
+        self.conv1 = nn.Conv2d(3, self.in_channel, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channel)
+        self.relu = nn.ReLU(inplace=True)  # inplace表示是否将原来值直接替换为激活后的值（不用中间变量进行存储）
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # 创建中间卷积层
+        self.layer1 = self._make_layer(block, 64, blocks_num[0])
+        self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, blocks_num[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, blocks_num[3], stride=2)
+
+        # 全连接，进行评估
+        if self.include_top:
+            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # output size = (1, 1) #自适应平均池化
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        # 权重初始化
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def _make_layer(self, block, channel, block_num, stride=1):
+        downsample = None  # 输入处理函数
+
+        # 输入通道与输出通道不相等，对输入通道进行增加或减少处理（保证正确地进行残差连接）
+        if stride != 1 or self.in_channel != channel * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channel, channel * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(channel * block.expansion))
+
+        layers = []
+        # 当前layer的第一个基础块
+        layers.append(block(self.in_channel,
+                            channel,
+                            downsample=downsample,
+                            stride=stride,
+                            groups=self.groups,
+                            width_per_group=self.width_per_group))
+
+        # 第一个基础块结束后，其输出为下一个基础块的输入（后面几个块中的第一层输入的通道数均相同）
+        self.in_channel = channel * block.expansion
+
+        for _ in range(1, block_num):
+            layers.append(block(self.in_channel,
+                                channel,
+                                groups=self.groups,
+                                width_per_group=self.width_per_group))
+
+        # 将当前layer中的块按顺序连接在一起（上一个块的输出为下一个块的输入）
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        # 首部处理
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        #print(x.shape)
+
+        # 卷积层计算
+        x = self.layer1(x)
+        # print('*'*50)
+        x = self.layer2(x)
+        # print('*' * 50)
+        x = self.layer3(x)
+        # print('*' * 50)
+        x = self.layer4(x)
+        # print('*' * 50)
+
+        # 评估打分
+        if self.include_top:
+            x = self.avgpool(x)
+            # print(x.shape)
+            x = torch.flatten(x, 1)  # 前1维不变，后面维度展平
+            x = self.fc(x)
+            # print(x.shape)
+
+        return x
+
+
+# # resnet34  pre-train parameters https://download.pytorch.org/models/resnet34-333f7ec4.pth
+# def resnet_samll(num_classes=1000, include_top=True):
+
+#     return ResNet(BasicBlock, [3, 4, 6, 3], num_classes=num_classes, include_top=include_top)
+
+# # resnet50  pre-train parameters https://download.pytorch.org/models/resnet50-19c8e357.pth
+# def resnet(num_classes=1000, include_top=True):
+#     return ResNet(Bottleneck, [3, 4, 6, 3], num_classes=num_classes, include_top=include_top)
+
+# # resnet101 pre-train parameters https://download.pytorch.org/models/resnet101-5d3b4d8f.pth
+# def resnet_big(num_classes=1000, include_top=True):
+#     return ResNet(Bottleneck, [3, 4, 23, 3], num_classes=num_classes, include_top=include_top)
+
+# # resneXt pre-train parameters https://download.pytorch.org/models/resnext50_32x4d-7cdf4587.pth
+# def resnext(num_classes=1000, include_top=True):
+#     groups = 32
+#     width_per_group = 4
+#     return ResNet(Bottleneck, [3, 4, 6, 3],
+#                   num_classes=num_classes,
+#                   include_top=include_top,
+#                   groups=groups,
+#                   width_per_group=width_per_group)
+
+# # resneXt_big pre-train parameters https://download.pytorch.org/models/resnext101_32x8d-8ba56ff5.pth
+# def resnext_big(num_classes=1000, include_top=True):
+#     groups = 32
+#     width_per_group = 8
+#     return ResNet(Bottleneck, [3, 4, 23, 3],
+#                   num_classes=num_classes,
+#                   include_top=include_top,
+#                   groups=groups,
+#                   width_per_group=width_per_group)
+
+def resnet34(num_classes=1000, include_top=True):
+    # https://download.pytorch.org/models/resnet34-333f7ec4.pth
+    return ResNet(BasicBlock, [3, 4, 6, 3], num_classes=num_classes, include_top=include_top)
+
+
+def resnet50(num_classes=1000, include_top=True):
+    # https://download.pytorch.org/models/resnet50-19c8e357.pth
+    return ResNet(Bottleneck, [3, 4, 6, 3], num_classes=num_classes, include_top=include_top)
+
+
+def resnet101(num_classes=1000, include_top=True):
+    # https://download.pytorch.org/models/resnet101-5d3b4d8f.pth
+    return ResNet(Bottleneck, [3, 4, 23, 3], num_classes=num_classes, include_top=include_top)
+
+
+def resnext50_32x4d(num_classes=1000, include_top=True):
+    # https://download.pytorch.org/models/resnext50_32x4d-7cdf4587.pth
+    groups = 32
+    width_per_group = 4
+    return ResNet(Bottleneck, [3, 4, 6, 3],
+                  num_classes=num_classes,
+                  include_top=include_top,
+                  groups=groups,
+                  width_per_group=width_per_group)
+
+
+def resnext101_32x8d(num_classes=1000, include_top=True):
+    # https://download.pytorch.org/models/resnext101_32x8d-8ba56ff5.pth
+    groups = 32
+    width_per_group = 8
+    return ResNet(Bottleneck, [3, 4, 23, 3],
+                  num_classes=num_classes,
+                  include_top=include_top,
+                  groups=groups,
+                  width_per_group=width_per_group)
+
+
+if __name__ == '__main__':
+    imgs_1 = torch.rand((5, 3, 224, 224))
+    # print(imgs_1)
+    # imgs_2 = torch.rand((5, 1, 2, 2))
+    # print(imgs_2)
+    # imgs_s = torch.cat([imgs_1, imgs_2], dim=1)
+    # print(imgs_s.shape)
+    # print(imgs_s)
+    bc = resnet34(8)
+    bc(imgs_1)
